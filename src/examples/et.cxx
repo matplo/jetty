71b57e4d2ab4b68060bdbf3c78e563f42d8aefc9
matplo <ploskon@gmail.com>
2017-06-30 09:45:19 -0700

    pdsf link jobs

--- src/examples/et.cxx
+++ src/examples/et.cxx
@@ -9,7 +9,10 @@
 
 #include <TFile.h>
 #include <TH1F.h>
+#include <TH2F.h>
 #include <TMath.h>
+#include <TNtuple.h>
+#include <TProfile.h>
 
 #include <string>
 #include <iostream>
@@ -31,16 +34,27 @@ int et (const std::string &s)
         }
         TFile *fout = TFile::Open(outfname.c_str(), "RECREATE");
         fout->cd();
-        TH1F *hpT = new TH1F("hpT", "pT;p_{T} (GeV/#it{c});counts", 50, 0, 100);
-        TH1F *het = new TH1F("hET", "hET;E_{T} (GeV);counts", 1000, 0, 1000.);
-        TH1F *hetmean = new TH1F("hETmean", "hETmean;E_{T}/2. (GeV);counts", 1000, 0, 1000.);
-        TH1F *het_diff = new TH1F("het_diff", "het_diff;#Delta E_{T} (GeV);counts", 1000, -1000, 1000.);
+        TH1F *hpt = new TH1F("hpt", "pt;p_{T} (GeV/#it{c});counts", 100, 0, 100);
+
+        TH1F *het = new TH1F("het", "het;E_{T} (GeV);counts", 100, 0, 100.);
+        TH1F *hetmean = new TH1F("hetmean", "hetmean;E_{T}/2 (GeV);counts", 100, 0, 100.);
+        TH1F *het_diff = new TH1F("het_diff", "het_diff;#Delta E_{T} (GeV);counts", 100, -100, 100.);
+
+        TH2F *hetmean2d = new TH2F("hetmean2d", "hetmean2d;#sqrt{s};E_{T}/2 (GeV);counts", 100, 0, 12000., 100, 0, 100.);
+        TH2F *hptmean2d = new TH2F("hptmean2d", "hptmean2d;#sqrt{s};p_{z}^{loss}/2 (GeV);counts", 100, 0, 12000., 100, 0, 100.);
+
+        TNtuple *tn = new TNtuple("eloss", "eloss", "ecm/F:p0/F:p1/F:et/F");
+
+        TProfile *p0 = new TProfile("p0", "p0", 12000, 0, 12000);
+        TProfile *p1 = new TProfile("p1", "p1", 12000, 0, 12000);
+        TProfile *et = new TProfile("et", "et", 12000, 0, 12000);
 
         // initialize pythia with a config and command line args
 		Pythia8::Pythia *ppythia = PyUtil::make_pythia(args.asString());
 		Pythia8::Pythia &pythia  = *ppythia;
 		auto &event              = pythia.event;
 
+        double ecm = args.getD("Beams:eCM");
 		// this is where the event loop section starts
         auto nEv = args.getI("Main:numberOfEvents");
         LoopUtil::TPbar pbar(nEv);
@@ -48,27 +62,36 @@ int et (const std::string &s)
         {
         	pbar.Update();
             if (pythia.next() == false) continue;
-            double total_ET = 0.0;
+            double total_et = 0.0;
             // loop over particles in the event
             for (unsigned int ip = 0; ip < event.size(); ip++)
             {
                 if (event[ip].isFinal())
                 {
                     if (TMath::Abs(event[ip].eta()) < 1.)
-                    	hpT->Fill(event[ip].pT(), 1./event[ip].pT());
-                    total_ET += event[ip].eT();
+                    	hpt->Fill(event[ip].pT(), 1./event[ip].pT());
+                    total_et += event[ip].eT();
                 }
             }
-            het->Fill(total_ET);
             PyUtil::OutKinematics outk(event);
             double mean_p_loss_perN = (outk.d_p_z[0] + outk.d_p_z[1])/2.;
-            double mean_eT_perN = total_ET/2.;
+            double mean_eT_perN = total_et/2.;
             double delta_means = mean_p_loss_perN - mean_eT_perN;
             //cout << "[i] mean_p_loss_perN = " << mean_p_loss_perN << " "
             //    << " tot_eT_perN = " << mean_eT_perN
             //    << " delta_means = " << delta_means
             //    << endl;
+            het->Fill(total_et);
+            hetmean->Fill(total_et/2.);
             het_diff->Fill(delta_means);
+
+            hetmean2d->Fill(ecm, total_et/2.);
+            hptmean2d->Fill(ecm, mean_p_loss_perN);
+
+            tn->Fill(ecm, outk.d_p_z[0], outk.d_p_z[1], total_et/2.);
+            p0->Fill(ecm, outk.d_p_z[0]);
+            p1->Fill(ecm, outk.d_p_z[1]);
+            et->Fill(ecm, total_et/2.);
         }
         pythia.stat();
         cout << "[i] Generation done." << endl;
--- tests/run_delta_e_check.sh
+++ tests/run_delta_e_check.sh
@@ -2,7 +2,12 @@
 
 module load jetty/default
 echo $JETTYDIR
-for e in 100 # 200 500 1000 1500 2000 3000 5000
+for e in 10 30 50 80 100 200 500 1000 1500 2000 2500 3000 3500 4000 4500 5000 6500 8000 9500
 do
-	jettyExamplesExe --et --minbias --output=et$e.root --nev=1000 Beams:eCM=$e
+	#jettyExamplesExe --et --minbias --output=et$e.root --nev=1000 Beams:eCM=$e
+	jettyExamplesExe --et --inel --output=et$e.root --nev=1000 Beams:eCM=$e
 done
+
+rm et_sum.root
+hadd -f et_sum.root et*.root
+root -l show.C -q
