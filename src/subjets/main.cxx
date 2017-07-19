ced5e712436307ae6c99241356c69bce08321590
matplo <ploskon@gmail.com>
2017-07-18 23:40:13 -0700

    subjets

--- src/subjets/main.cxx
+++ src/subjets/main.cxx
@@ -1,16 +1,22 @@
 #include <util/args.h>
 #include "run_pythia.h"
+#include "run_jets.h"
 
 int main ( int argc, char *argv[] )
 {
 	int rv = 0;
 
-    SysUtil::Args args(argc, argv);
+	SysUtil::Args args(argc, argv);
 
-    if (args.isSet("--pythia"))
-    {
-    	rv = run_pythia(args.asString());
-    }
+	if (args.isSet("--pythia"))
+	{
+		rv = run_pythia(args.asString());
+	}
 
-    return rv;
+	if (args.isSet("--jets"))
+	{
+		rv = run_jets(args.asString());
+	}
+
+	return rv;
 }
