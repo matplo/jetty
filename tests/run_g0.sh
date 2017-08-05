024a905128b283907c70fa823c9421324cae0029
matplo <ploskon@gmail.com>
2017-08-04 19:46:06 -0700

    g0

--- tests/run_g0.sh
+++ tests/run_g0.sh
@@ -9,7 +9,7 @@ do
 	ibhat=$((2+counter*counter))
 	ibhatMax=$((2+(counter+1)*(counter+1)))
 	echo $ibhat $ibhatMax
-	jettyExamplesExe --pythia-wrapper --overwrite --hconfig=h_pythia_parts_g0.cfg --nev=500 --output=g0.root \
+	jettyExamplesExe --pythia-wrapper --overwrite --hconfig=h_pythia_parts_g0.cfg --nev=500 --out=g0.root \
 		PhaseSpace:pTHatMin=$ibhat PhaseSpace:pTHatMax=$ibhatMax \
 		HardQCD:all=on PromptPhoton:all=off
 	counter=$((counter+1))
