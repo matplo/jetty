8a25b29f5d981a5472229ea56687916142c4576f
matplo <ploskon@gmail.com>
2017-08-18 13:36:34 -0700

    small fixes

--- scripts/bjetty.sh
+++ scripts/bjetty.sh
@@ -2,13 +2,13 @@
 
 cd $(dirname $BASH_SOURCE)
 BT_config=./jetty.cfg
-source $HOME/devel/buildtools/bt.sh "$@" --build
+source $HOME/devel/buildtools/bt.sh "$@"
 
 function build()
 {
 	cd ${BT_build_dir}
-	echo ${BT_src_dir}
-    cmake -DCMAKE_INSTALL_PREFIX=${BT_install_dir} -DCMAKE_BUILD_TYPE=${BT_release_type} ${BT_src_dir}
+	echo "[i] building sources at ${BT_src_dir}"
+    cmake -DCMAKE_INSTALL_PREFIX=${BT_install_dir} -DCMAKE_BUILD_TYPE=${BT_build_type} ${BT_src_dir}
     [ ${BT_clean} ] && make clean
     make -j $(n_cores) VERBOSE=$BT_verbose
     make install
