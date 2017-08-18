42da1afb2f2f0284cad1b43e90c4f5257c35c406
matplo <ploskon@gmail.com>
2017-08-17 18:50:02 -0700

    savegame

--- scripts/bjetty.sh
+++ scripts/bjetty.sh
@@ -7,6 +7,7 @@ source $HOME/devel/buildtools/bt.sh "$@" --build
 function build()
 {
 	cd ${BT_build_dir}
+	echo ${BT_src_dir}
     cmake -DCMAKE_INSTALL_PREFIX=${BT_install_dir} -DCMAKE_BUILD_TYPE=${BT_release_type} ${BT_src_dir}
     [ ${BT_clean} ] && make clean
     make -j $(n_cores) VERBOSE=$BT_verbose
