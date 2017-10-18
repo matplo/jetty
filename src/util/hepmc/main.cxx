4cd9eb795a6f7ac20e9b226b682b45d9c1cbf6bb
matplo <ploskon@gmail.com>
2017-10-18 16:59:41 -0700

    test read particles

--- src/util/hepmc/main.cxx
+++ src/util/hepmc/main.cxx
@@ -6,6 +6,14 @@ int main ( int argc, char *argv[] )
 {
 	SysUtil::Args args(argc, argv);
 	Linfo << args.asString();
+
+	ReadHepMCFile f(args.get("--file").c_str());
+	while (f.NextEvent())
+	{
+		Linfo << "number of particles:" << f.HepMCParticles(false).size();
+		Linfo << "number of final particles:" << f.HepMCParticles(true).size();
+	}
+
 	return 0;
 };
 
