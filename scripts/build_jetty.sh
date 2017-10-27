280d6f30f8d88904f41781946c4e3a80808e6b97
matplo <ploskon@gmail.com>
2017-10-27 15:42:09 -0700

    update of the build

--- scripts/build_jetty.sh
+++ scripts/build_jetty.sh
@@ -1,48 +1,13 @@
 #!/bin/bash
 
-args=$@
-function is_arg_set
-{
-        for i in $args ; do
-            if [[ $i == $1 ]] ; then
-                return 0 #this is true
-            fi
-        done
-        return 1 #this is false
-}
+cd $(dirname $BASH_SOURCE)
 
-savedir=$PWD
-
-if [ -z $JETTYDIR ]; then
-  echo "[e] JETTYDIR dir not set..."
-  exit
+#[ ! -f ./bt.sh ] && wget https://raw.github.com/matplo/buildtools/master/bt.sh && chmod +x ./bt.sh
+# [ ! -f ./bt.sh ] && wget --no-check-certificate https://raw.github.com/matplo/buildtools/master/bt.sh && chmod +x ./bt.sh
+if [[ ! -f ./.bt/bt.sh ]]; then
+	git clone git@github.com:matplo/buildtools.git .bt
 fi
+# cp -v ~/devel/buildtools/bt.sh .
 
-if [ -d $JETTYDIR ]; then
-  cd $JETTYDIR
+[ -f ./.bt/bt.sh ] && ./.bt/bt.sh BT_script=build.sh $@
 
-  if is_arg_set "realclean" ;
-  then
-    rm -rf $JETTYDIR/include/* $JETTYDIR/lib/* $JETTYDIR/bin/*
-  fi
-
-  for pack in src
-  do
-    bdir=$JETTYDIR/.build/$pack
-    if is_arg_set "realclean" ;
-    then
-    	rm -rf $bdir
-    fi
-    mkdir -p $bdir
-    cd $bdir
-    debug="-DCMAKE_BUILD_TYPE=Release"
-    #cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo
-    #is_arg_set "o3" && export CXXFLAGS=-O3 # Release does it
-    is_arg_set "debug" && debug=-DCMAKE_BUILD_TYPE=Debug
-    cmake -DCMAKE_INSTALL_PREFIX=$JETTYDIR $debug $JETTYDIR/$pack
-    is_arg_set "clean" && make clean
-    is_arg_set "verbose" && verbose="VERBOSE=1"
-    make $verbose && make install
-  done
-fi
-cd $savedir
