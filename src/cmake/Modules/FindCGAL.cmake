6a4a72b4569488ed54185448caf0cf1a6064e673
matplo <ploskon@gmail.com>
2017-10-28 14:52:57 -0700

    cgal still

--- src/cmake/Modules/FindCGAL.cmake
+++ src/cmake/Modules/FindCGAL.cmake
@@ -22,6 +22,19 @@ else(CGAL_INCLUDE_DIR AND CGAL_LIBRARIES)
                  $ENV{CGAL_ROOT}/lib
                  $ENV{CGAL_DIR}/lib64
                  $ENV{CGAL_ROOT}/lib64)
+    else()
+    find_path(CGAL_INCLUDE_DIR CGAL/basic.h
+              HINTS
+              /usr/local/include
+              /usr/include)
+
+    find_library(CGAL_LIBRARIES NAMES CGAL libCGAL
+                 HINTS
+                 /usr/lib
+                 /usr/lib64
+                 /usr/local/lib
+                 /usr/local/lib64)
+
   endif((IS_DIRECTORY ${CGAL_ROOT}) OR (IS_DIRECTORY $ENV{CGAL_DIR}))
 
   mark_as_advanced(CGAL_INCLUDE_DIR CGAL_LIBRARIES)
