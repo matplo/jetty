aeb5603ef16973792330cea924089683f0cbfbf2
matplo <ploskon@gmail.com>
2017-11-13 17:12:39 -0800

    put some things into a tree

--- src/area/simple.cxx
+++ src/area/simple.cxx
@@ -3,10 +3,12 @@
 #include <util/strutil.h>
 #include <util/looputil.h>
 #include <util/hepmc/readfile.h>
+#include <util/rstream/tstream.h>
 
 #include <TFile.h>
 #include <TProfile.h>
 #include <TRandom.h>
+#include <TTree.h>
 
 #include <string>
 
@@ -22,15 +24,15 @@ int simple ( int argc, char *argv[] )
 	SysUtil::Args args(argc, argv);
 	Linfo << args.asString();
 
-	double etamax = TMath::Abs(args.getD("--etamax", 0.9));
-	string sfoutputname = args.get("--output", "voronoi_area_incident_t2.root");
-	if (sfoutputname == "voronoi_area_incident_t2.root")
+	double etamax = TMath::Abs(args.getD("--etamax", 1.0));
+	string sfoutputname = args.get("--output", "voronoi_area_simple_tree.root");
+	if (sfoutputname == "voronoi_area_simple_tree.root")
 	{
 		string ext = StrUtil::sT("_etamax_") + StrUtil::sT(etamax) + StrUtil::sT(".root");
 		boost::algorithm::replace_last(sfoutputname, ".root", ext);
 	}
-
 	double deta = etamax * 2.;
+	double acceptance = deta * 2.*TMath::Pi();
 	Long64_t nghosts = TMath::Abs(args.getI("--nghost-eta", 100)) *deta;
 	if (args.isSet("--fixed-nghost"))
 	{
@@ -56,7 +58,7 @@ int simple ( int argc, char *argv[] )
 		string ext = StrUtil::sT("_random.root");
 		boost::algorithm::replace_last(sfoutputname, ".root", ext);
 	}
-	VoronoiUtil::EventAreaSetup ea_setup(etamax, TMath::Abs(args.getI("--nghost-eta", 100)), min_ghost_area, args.isSet("--fixed_nghosts"));
+	VoronoiUtil::EventAreaSetup ea_setup(etamax, TMath::Abs(args.getI("--nghost-eta", 0)), min_ghost_area, args.isSet("--fixed_nghosts"));
 	ea_setup.Dump();
 
 	Stat_t nevents = args.getI("--nev", 0);
@@ -66,17 +68,8 @@ int simple ( int argc, char *argv[] )
 
 	Linfo << "output goes to : " << sfoutputname;
 	TFile *fout = new TFile(sfoutputname.c_str(), "recreate");
-	TProfile *hpmult = new TProfile("hpmult", "hpmult", 100, 0, 100);
-	TProfile *hpareaM = new TProfile("hpareaM", "hpareaM", 100, 0, max_mult);
-	TProfile *hptotareaM = new TProfile("hptotareaM", "hptotareaM", 100, 0, max_mult);
-	TProfile *hptotareasfractionM = new TProfile("hptotareasfractionM", "hptotareasfractionM", 100, 0, max_mult);
-
-	TProfile *hpareaghostM = new TProfile("hpareaghostM", "hpareaghostM", 100, 0, max_mult);
-	TProfile *hptotareaghostM = new TProfile("hptotareaghostM", "hptotareaghostM", 100, 0, max_mult);
-	TProfile *hptotareaghostfractionM = new TProfile("hptotareaghostfractionM", "hptotareaghostfractionM", 100, 0, max_mult);
-
-	TProfile *hptotareasfractionMdeta = new TProfile("hptotareasfractionMdeta", "hptotareasfractionMdeta", 100, 0, max_mult / deta);
-	TProfile *hptotareaghostfractionMdeta = new TProfile("hptotareaghostfractionMdeta", "hptotareaghostfractionMdeta", 100, 0, max_mult / deta);
+	TTree *t = new TTree("t", "t");
+	RStream::TStream astream("va", t);
 
 	ReadHepMCFile f(args.get("--file").c_str());
 	while (f.NextEvent())
@@ -84,6 +77,8 @@ int simple ( int argc, char *argv[] )
 		pbar.Update();
 		// Ldebug << "number of particles:" << f.HepMCParticles(false).size();
 		// Ldebug << "number of final particles:" << f.HepMCParticles(true).size();
+
+		double total_pt = 0.0;
 
 		std::vector<HepMC::GenParticle*> particles = f.HepMCParticles(true);
 		std::vector<VoronoiUtil::point_2d_t> particle_reco_area_estimation;
@@ -95,6 +90,7 @@ int simple ( int argc, char *argv[] )
 			// Ltrace << "eta of a particle: " << v4.eta();
 			if (TMath::Abs(v4.eta()) < etamax)
 			{
+				total_pt += v4.perp();
 				if (random_flag == false)
 				{
 					particle_reco_area_estimation.push_back( VoronoiUtil::point_2d_t(v4.eta(), v4.phi()));
@@ -108,37 +104,25 @@ int simple ( int argc, char *argv[] )
 			}
 		}
 
-		double multiplicity = double(particle_reco_area_estimation.size());
-		//Ldebug << "multiplicity of final particles in |eta| <" << etamax << " :" << multiplicity;
-		hpmult->Fill(pbar.NCalls() % 100, multiplicity);
+		if (particle_reco_area_estimation.size() == 0) continue;
 
+		double multiplicity = double(particle_reco_area_estimation.size());
 		double total_reco_area = 0.0;
 		double total_reco_area_ghost = 0.0;
-		//VoronoiUtil::EventArea ea(particle_reco_area_estimation, -1);
-		// VoronoiUtil::EventArea ea(ea_setup, particle_reco_area_estimation, ghosts);
 		VoronoiUtil::EventArea ea(ea_setup, particle_reco_area_estimation, true);
 		ea.TotalArea(total_reco_area, total_reco_area_ghost);
 
-		hptotareaM->Fill(multiplicity, total_reco_area);
-		hptotareasfractionM->Fill(multiplicity, total_reco_area);
-		hptotareasfractionMdeta->Fill(multiplicity/deta, total_reco_area);
-
-		hptotareaghostM->Fill(multiplicity, total_reco_area_ghost);
-		hptotareaghostfractionM->Fill(multiplicity, total_reco_area_ghost);
-		hptotareaghostfractionMdeta->Fill(multiplicity/deta, total_reco_area_ghost);
+		astream << "deta" << deta;
+		astream << "mult" << multiplicity;
+		astream << "total_pt" << total_pt;
+		astream << "a_parts" << total_reco_area;
+		astream << "a_ghost" << total_reco_area_ghost;
+		astream << "acc" << acceptance;
+		astream << endl;
 
 		if (nevents > 0 && pbar.NCalls() >= nevents) break;
 	}
 
-	double acceptance = 2.*etamax*2.*TMath::Pi();
-	// hptotareasfractionM->Sumw2();
-	if (etamax>0)
-	{
-		hptotareasfractionM->Scale(1./acceptance);
-		hptotareasfractionMdeta->Scale(1./acceptance);
-		hptotareaghostfractionM->Scale(1./acceptance);
-		hptotareaghostfractionMdeta->Scale(1./acceptance);
-	}
 	fout->Write();
 	fout->Close();
 	delete fout;
