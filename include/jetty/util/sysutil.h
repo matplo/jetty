fbab4727e9d0637eecadc750ce4c9a2e336ec2ac
matplo <ploskon@gmail.com>
2018-02-23 16:30:58 -0800

    find_files with boost::

--- include/jetty/util/sysutil.h
+++ include/jetty/util/sysutil.h
@@ -4,10 +4,21 @@
 #include <string>
 #include <sys/stat.h>
 
+#define BOOST_FILESYSTEM_NO_DEPRECATED
+#define BOOST_FILESYSTEM_VERSION 3
+
+#include <boost/filesystem.hpp>
+
 namespace SysUtil
 {
 	bool file_exists (const std::string& name);
 	// std::string expand_path_name(const std::string& spath);
+
+	bool find_file(const boost::filesystem::path& dir_path, const boost::filesystem::path& file_name, boost::filesystem::path& path_found);
+
+	std::vector<std::string> find_files_ext(const char *dir, const char* ext);
+	std::vector<std::string> find_files_exact(const char *dir, const char* exact);
+
 }
 
 #endif
--- src/util/sysutil.cxx
+++ src/util/sysutil.cxx
@@ -8,6 +8,9 @@
 //#include <boost/filesystem.hpp>
 //namespace fs = boost::filesystem;
 
+#include <algorithm>
+#include <iostream>
+
 namespace SysUtil
 {
 	bool file_exists (const std::string& name)
@@ -21,4 +24,62 @@ namespace SysUtil
 	// 	std::vector<std::string> v;
 	// 	return v;
 	// }
+
+
+	bool find_file(const boost::filesystem::path& dir_path, const boost::filesystem::path& file_name, boost::filesystem::path& path_found)
+	{
+		using namespace std;
+		using namespace boost::filesystem;
+
+		const recursive_directory_iterator end;
+		const auto it = find_if(recursive_directory_iterator(dir_path), end,
+		                        [&file_name](const directory_entry& e)
+		                        { return e.path().filename() == file_name;});
+		if (it == end) {
+		return false;
+		} else {
+		path_found = it->path();
+		return true;
+		}
+	}
+
+	//http://www.technical-recipes.com/2014/using-boostfilesystem/#Iterating
+	std::vector<std::string> find_files_ext(const char *dir, const char* ext)
+	{
+		boost::filesystem::recursive_directory_iterator rdi(dir);
+		boost::filesystem::recursive_directory_iterator end_rdi;
+
+		std::vector<std::string> found_files;
+		std::string ext_str0(ext);
+		for (; rdi != end_rdi; rdi++)
+		{
+			//rdi++;
+
+			if (ext_str0.compare((*rdi).path().extension().string()) == 0)
+			{
+				found_files.push_back((*rdi).path().string());
+			}
+		}
+		return found_files;
+	}
+
+	std::vector<std::string> find_files_exact(const char *dir, const char* exact)
+	{
+		boost::filesystem::recursive_directory_iterator rdi(dir);
+		boost::filesystem::recursive_directory_iterator end_rdi;
+
+		std::vector<std::string> found_files;
+		std::string sexact(exact);
+		for (; rdi != end_rdi; rdi++)
+		{
+			//rdi++;
+
+			if (sexact.compare((*rdi).path().filename().string()) == 0)
+			{
+				found_files.push_back((*rdi).path().string());
+			}
+		}
+		return found_files;
+	}
+
 }
