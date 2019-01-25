56e4a62f9a7ee6c2cfd0b0e1a7e7972cd6940606
matplo <ploskon@gmail.com>
2019-01-24 16:29:43 -0800

    iterate over hep mc event; beam particles and vertices

--- src/eic/eicgeneraltask.cxx
+++ src/eic/eicgeneraltask.cxx
@@ -25,6 +25,10 @@
 #include <fastjet/ClusterSequence.hh>
 namespace fj = fastjet;
 
+#include <HepMC/GenRanges.h>
+#include <HepMC/GenParticle.h>
+#include <boost/foreach.hpp>
+
 namespace EIC
 {
 	GeneralTask::Settings::Settings() :
@@ -164,6 +168,61 @@ namespace EIC
 
 		ps << "hepmc_reread_" << fMCEvWrapper->PseudoJetParticles(true);
 
+		Linfo << "hepmc beam particles: " << fMCEvWrapper->HepMCParticles(false, 4).size() << endl;
+		Linfo << "hepmc outgoing particles final: " << fMCEvWrapper->HepMCParticles(true, 1).size() << endl;
+		// for (auto p : fMCEvWrapper->HepMCParticles(false))
+		// {
+		// 	if (p->is_beam() && p->pdg_id() == 11)
+		// 	{
+		// 		// BOOST_FOREACH(HepMC::GenParticle *p, p->particles_out_const_iterator())
+		// 		// {
+		// 		// 	Linfo << " - " << p->pdg_id();
+		// 		// }
+		// 		//; //Linfo << p->pdg_id() << " n daughters: " << p->numberOfDaughters();
+		// 		HepMC::GenVertex *v = p->end_vertex();
+		// 		for ( HepMC::GenVertex::particle_iterator _p = v->particles_begin(); p != v->particles_end(); ++p )
+		// 		{
+		// 			HepMC::GenParticle* pmc = *_p;
+		// 		}
+		// 		// auto it = v->particles_out_const_iterator();
+		// 		// for (auto px : it)
+		// 		// 	Linfo << px;
+		// 	}
+		// }
+
+		// good links to iteration tests: http://lcgapp.cern.ch/project/simu/HepMC/205/html/classHepMC_1_1GenVertex_1_1particle__iterator.html#_details
+		Linfo << "find the outgoing electron...";
+        for ( HepMC::GenEvent::particle_iterator p = fMCEvWrapper->GetEvent()->particles_begin();
+             p != fMCEvWrapper->GetEvent()->particles_end();
+             ++p )
+        {
+        	// find the beam electron
+			if ((*p)->is_beam() && (*p)->pdg_id() == 11)
+			{
+				Linfo << "beam and an an electron status=" << (*p)->status();
+				(*p)->print( std::cout );
+				if ( (*p)->end_vertex() )
+				{
+					for ( HepMC::GenVertex::particle_iterator des =(*p)->end_vertex()->particles_begin(HepMC::descendants);
+					     des != (*p)->end_vertex()->particles_end(HepMC::descendants);
+					     ++des )
+					{
+						// look for a final state electron
+						if ((*des)->pdg_id() == 11 && (*des)->status() == 1)
+						{
+							Linfo << "descendant and an electron status=" << (*des)->status();
+							(*des)->print( std::cout );
+						}
+					}
+				}
+			}
+		}
+
+		Linfo << " -- ";
+		// Linfo << "number of vertices: " << fMCEvWrapper->Vertices().size();
+
+
+		// Linfo << "hepmc outgoing particle: " << fMCEvWrapper->HepMCParticles(false, 63)[0]->pdg_id() << endl;
 		// for (auto p : parts)
 		// {
 		// 	if (TMath::Abs(p.eta()) > fSettings.maxEta)
